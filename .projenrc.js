bab17ab2183d0b92127e5fb1c259fed687166433
Christopher Rybicki <crybicki98@gmail.com>
2022-03-20 01:38:41 -0700

    disable mergify

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { awscdk } = require('projen');
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
@@ -8,5 +9,8 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  mergify: false,
 });
+
 project.synth();
